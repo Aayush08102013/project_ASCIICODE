deb0be8069b23217bcf86cc5dd8d5808d49aba85
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-03-20 13:24:57 +1300

    Add files via upload

---
+++ project_ASCIICODE.py
@@ -0,0 +1,111 @@
+i = str(input("enter you alphabetic letter: "))
+if i == "a":
+    print(f"the ASCII value of {i} is 97.")
+elif i == "A":
+    print(f"the ASCII value of {i} is 65.")
+elif i == "b":
+    print(f"the ASCII value of {i} is 98.")
+elif i == "B":
+    print(f"the ASCII value of {i} is 66.")
+elif i == "c":
+    print(f"the ASCII value of {i} is 99.")
+elif i == "C":
+    print(f"the ASCII value of {i} is 67.")
+elif i == "d":
+    print(f"the ASCII value of {i} is 100.")
+elif i == "D":
+    print(f"the ASCII value of {i} is 68.")
+elif i == "e":
+    print(f"the ASCII value of {i} is 101.")
+elif i == "E":
+    print(f"the ASCII value of {i} is 69.")
+elif i == "f":
+    print(f"the ASCII value of {i} is 102.")
+elif i == "F":
+    print(f"the ASCII value of {i} is 70.")
+elif i == "g":
+    print(f"the ASCII value of {i} is 103.")
+elif i == "G":
+    print(f"the ASCII value of {i} is 71.")
+elif i == "h":
+    print(f"the ASCII value of {i} is 104.")
+elif i == "H":
+    print(f"the ASCII value of {i} is 72.")
+elif i == "h":
+    print(f"the ASCII value of {i} is 104.")
+elif i == "H":
+    print(f"the ASCII value of {i} is 72.")
+elif i == "i":
+    print(f"the ASCII value of {i} is 105.")
+elif i == "I":
+    print(f"the ASCII value of {i} is 73.")
+elif i == "j":
+    print(f"the ASCII value of {i} is 106.")
+elif i == "J":
+    print(f"the ASCII value of {i} is 74.")
+elif i == "k":
+    print(f"the ASCII value of {i} is 107.")
+elif i == "K":
+    print(f"the ASCII value of {i} is 75.")
+elif i == "l":
+    print(f"the ASCII value of {i} is 108.")
+elif i == "L":
+    print(f"the ASCII value of {i} is 76.")
+elif i == "m":
+    print(f"the ASCII value of {i} is 109.")
+elif i == "M":
+    print(f"the ASCII value of {i} is 77.")
+elif i == "n":
+    print(f"the ASCII value of {i} is 110.")
+elif i == "N":
+    print(f"the ASCII value of {i} is 78.")
+elif i == "o":
+    print(f"the ASCII value of {i} is 111.")
+elif i == "O":
+    print(f"the ASCII value of {i} is 79.")
+elif i == "p":
+    print(f"the ASCII value of {i} is 112.")
+elif i == "P":
+    print(f"the ASCII value of {i} is 80.")
+elif i == "q":
+    print(f"the ASCII value of {i} is 113.")
+elif i == "Q":
+    print(f"the ASCII value of {i} is 81.")
+elif i == "r":
+    print(f"the ASCII value of {i} is 114.")
+elif i == "R":
+    print(f"the ASCII value of {i} is 82.")
+elif i == "s":
+    print(f"the ASCII value of {i} is 115.")
+elif i == "S":
+    print(f"the ASCII value of {i} is 83.")
+elif i == "t":
+    print(f"the ASCII value of {i} is 116.")
+elif i == "T":
+    print(f"the ASCII value of {i} is 84.")
+elif i == "u":
+    print(f"the ASCII value of {i} is 117.")
+elif i == "U":
+    print(f"the ASCII value of {i} is 85.")
+elif i == "v":
+    print(f"the ASCII value of {i} is 118.")
+elif i == "V":
+    print(f"the ASCII value of {i} is 86.")
+elif i == "w":
+    print(f"the ASCII value of {i} is 119.")
+elif i == "W":
+    print(f"the ASCII value of {i} is 87.")
+elif i == "x":
+    print(f"the ASCII value of {i} is 120.")
+elif i == "X":
+    print(f"the ASCII value of {i} is 88.")
+elif i == "y":
+    print(f"the ASCII value of {i} is 121.")
+elif i == "Y":
+    print(f"the ASCII value of {i} is 89.")
+elif i == "z":
+    print(f"the ASCII value of {i} is 122.")
+elif i == "Z":
+    print(f"the ASCII value of {i} is 90.")
+else:
+    print(f"{i} is an ivalid input")
